9e85d2a1fcdaa8af488e6970de1c03242277e0af
puzzleton <puzzleton@gmail.com>
2024-11-03 21:52:03 -0600

    Added password checker logic
    Co-authored-by: James M Quinones <Jimmy-Quinones@users.noreply.github.com>

--- day4.py
+++ day4.py
@@ -1,6 +1,43 @@
 input = '272091-815432'
+range_start, range_end = input.split('-')
+
+VALID_PASSWORD = 5
+GENERIC_INVALID_PASSWORD = -1
 
 output = 0
+
+# def parseInputIntoStrings():
+
+def passwordChecker(password):
+  # '133456' -> ['1', '3', '3', '4', '5', '6']
+  characters = list(password)
+
+  # INVALID: Length not 6
+  if len(characters) != 6:
+    return -1
+  
+  # INVALID: Password out of range
+  if password < range_start or password > range_end:
+    return -1
+
+  # INVALID: No repeats or descending value or triplet
+  is_repeat_present = False
+  # Loop through characters, comparing digit ahead for ascending value or repeat
+  for index, value in enumerate(characters):
+    if index < len(characters) - 1:
+      value_ahead_one_position = characters[index + 1]
+      if value > value_ahead_one_position:
+        return index
+      # if index < len(characters) - 2:
+      #   value_ahead_two_positions = characters[index + 2]
+      #   if value == value_ahead_one_position and value == value_ahead_two_positions:
+      #     return False
+      if value == value_ahead_one_position:
+        is_repeat_present = True
+    else:
+      if not(is_repeat_present):
+        return -1
+  return 5
 
 # Example steps to find the next valid password:
 # Check 272091 as our start input (*** = INVALID)
@@ -10,15 +47,20 @@ output = 0
 # 277789
 # ***277790*** copy -> 277799
 # ***277800*** copy -> 277888
+def findNextValidPassword(password):
+  return str(int(password) + 1)
 
-# def parseInputIntoNumbers():
+def main():
+  valid_counter = 0
+  current_password = range_start
+  first_result = passwordChecker(current_password)
+  if first_result == VALID_PASSWORD:
+    valid_counter += 1
+  while current_password < range_end:
+    next_password = findNextValidPassword(current_password)
+    if next_password <= range_end:
+      valid_counter += 1
+    current_password = next_password
+  print(valid_counter)
 
-# def passwordChecker(number):
-
-# def findNextValidPassword(number): ??
-
-# Parse the input
-# Check the range start is valid password
-# Find the next valid password (if possible)
-# If another valid password is found, increase the output by 1 and check for another valid password
-# If another valid password is not found, abort
+main()
