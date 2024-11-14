3cc17012aabc3dab2b9944890a94d47885b00dbd
puzzleton <puzzleton@gmail.com>
2024-11-13 20:26:21 -0600

    Finalized a brute force loop and finished part 2
    Co-authored-by: James M Quinones <Jimmy-Quinones@users.noreply.github.com>

--- day4.py
+++ day4.py
@@ -8,17 +8,20 @@ output = 0
 
 # def parseInputIntoStrings():
 
+# 133456
+# 3 3 3 5 8 8
+
 def passwordChecker(password):
   # '133456' -> ['1', '3', '3', '4', '5', '6']
   characters = list(password)
 
   # INVALID: Length not 6
   if len(characters) != 6:
-    return -1
+    return GENERIC_INVALID_PASSWORD
   
   # INVALID: Password out of range
   if password < range_start or password > range_end:
-    return -1
+    return GENERIC_INVALID_PASSWORD
 
   # INVALID: No repeats or descending value or triplet
   is_repeat_present = False
@@ -26,19 +29,26 @@ def passwordChecker(password):
   for index, value in enumerate(characters):
     if index < len(characters) - 1:
       value_ahead_one_position = characters[index + 1]
+      # We detect a descending value
       if value > value_ahead_one_position:
         return index
-      # if index < len(characters) - 2:
-      #   value_ahead_two_positions = characters[index + 2]
-      #   if value == value_ahead_one_position and value == value_ahead_two_positions:
-      #     return False
-      if value == value_ahead_one_position:
+      # We find the surrounding values
+      value_behind_one_position = None
+      value_ahead_two_positions = None
+      if index > 0:
+        value_behind_one_position = characters[index - 1]
+      if index < len(characters) - 2:
+        value_ahead_two_positions = characters[index + 2]
+
+      # We detect a double
+      if value == value_ahead_one_position and value != value_ahead_two_positions and value != value_behind_one_position:
         is_repeat_present = True
     else:
       if not(is_repeat_present):
-        return -1
-  return 5
+        return GENERIC_INVALID_PASSWORD
+  return VALID_PASSWORD
 
+# Potential for optimization by calculating next valid password instead of incrementing by 1
 # Example steps to find the next valid password:
 # Check 272091 as our start input (*** = INVALID)
 # Find the next available password by copying the digit before the first descending value (277777)
@@ -58,7 +68,7 @@ def main():
     valid_counter += 1
   while current_password < range_end:
     next_password = findNextValidPassword(current_password)
-    if next_password <= range_end:
+    if next_password <= range_end and passwordChecker(next_password) == VALID_PASSWORD:
       valid_counter += 1
     current_password = next_password
   print(valid_counter)
